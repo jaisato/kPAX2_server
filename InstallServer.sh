4038cfacf615a7b87b1e7186ec214fef93960839
Jairo Sarabia <jairosarabia@gmail.com>
2018-06-30 22:26:57 +0200

    Shell script adapted to Vagrant installation.

--- InstallServer.sh
+++ InstallServer.sh
@@ -12,36 +12,36 @@
 # NPM
 clear
 echo "Install NPM & nodejs"
-sudo aptitude install npm nodejs-legacy 
-read -rsp $'Press any key to continue...\n' -n1
+
+sudo aptitude -y install npm nodejs-legacy
 
 # MongoDB
 clear
 echo "Install MongoDB"
+
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-sudo apt-get update
+sudo apt-get -y update
 sudo apt-get install -y mongodb-org
 sudo service mongod start
-read -rsp $'Press any key to continue...\n' -n1
-
 
 # NPM modules
 clear
 echo "Install NPM Modules"
+
 npm install express serve-favicon morgan cookie-parser body-parser debugging mongodb debug pug jade
-read -rsp $'Press any key to continue...\n' -n1
 
 # Execute server
 clear
+
 echo "Export variables"
 echo "export MONGODB_URL=\"mongodb://readwrite:1234@ds021462.mlab.com:21462/kpax2\""
 echo "export DEBUG=* MONGODB_URL=\"mongodb://readwrite:1234@ds021462.mlab.com:21462/kpax2\""
+
 export MONGODB_URL="mongodb://readwrite:1234@ds021462.mlab.com:21462/kpax2"
 export DEBUG=* MONGODB_URL="mongodb://readwrite:1234@ds021462.mlab.com:21462/kpax2"
-echo ""
+
 echo "Executar el server"
 echo "npm start"
-read -rsp $'Press any key to continue...\n' -n1 
 
 exit 0
